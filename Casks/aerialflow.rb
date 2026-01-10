a91a61b9a3c2ef3a8a1e10b69271ba19dc2c093a
Floris Robbemont <floris@secondarrow.io>
2026-01-10 01:35:24 +0100

    aerialflow 1.0.8

--- Casks/aerialflow.rb
+++ Casks/aerialflow.rb
@@ -1,8 +1,8 @@
 cask "aerialflow" do
-  version "1.0.7"
-  sha256 "52b5d60d0a43c89b1cee8fc2db7950c787e18bb28119c7f2a52dbcd265fedfd4"
+  version "1.0.8"
+  sha256 "e8fc9e98d4de2bae997c854bdf4b56c88740a0c5e1fe26401e02491c5fd68be5"
 
-  url "https://github.com/second-arrow/aerialflow/releases/download/v1.0.7/AerialFlow-1.0.7-9-universal.dmg",
+  url "https://github.com/second-arrow/aerialflow/releases/download/v1.0.8/AerialFlow-1.0.8-10-universal.dmg",
       verified: "github.com/second-arrow/aerialflow/"
   name "AerialFlow"
   desc "Fixes macOS Aerial wallpaper rotation and adds screensaver controls"
